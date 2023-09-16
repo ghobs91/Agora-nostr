652f115b7a1b291ce7fdb488d3aa2f1b0b5d5b7c
ghobs91 <aghobrial@ptc.com>
2023-09-16 04:36:42 -0400

    imgur upload complete

--- src/partials/ImageInput.svelte
+++ src/partials/ImageInput.svelte
@@ -22,16 +22,21 @@
     if (input) {
       listener = listenForFile(input, async inputFile => {
         if (inputFile) {
-          const opts = filter(identity, {maxWidth, maxHeight})
+        //   const opts = filter(identity, {maxWidth, maxHeight})
 
-          file = blobToFile(await stripExifData(inputFile, opts))
-          // quote = await postJson(user.dufflepud("/upload/quote"), {
-          quote = await postJson(`https://api.imgur.com/3/image`, {
-            uploads: [{size: file.size}],
-          })
-        } else {
-          file = null
-          quote = null
+        //   file = blobToFile(await stripExifData(inputFile, opts))
+        //   // quote = await postJson(user.dufflepud("/upload/quote"), {
+        //   quote = await postJson(`https://api.imgur.com/3/image`, {
+        //     uploads: [{size: file.size}],
+        //   })
+        // } else {
+          // file = null
+          quote = {
+            termsLine1: `If someone else might own the copyright to it, don't upload it. Don't upload gore, "hate speech" (i.e. demeaning race, gender, age, religious or sexual orientation, etc.), or material that is threatening, harassing, defamatory, or that encourages violence or crime.`,
+            termsLine2: `Don't upload illegal content such as child sexual abuse material or nonconsensual ("revenge") porn. Don't hotlink to adult content or to file-sharing, gambling, torrent, warez, or Imgur rip-off sites. Don't impersonate someone else. Also, don't use Imgur to host image libraries you link to from elsewhere, content for your website, advertising, avatars, or anything else that turns us into your content delivery network.`,
+            termsLine3: `If you do – and we will be the judge – or if you do anything illegal, in addition to any other legal rights we may have, we will ban you along with the site you're hotlinking from, delete all your images, report you to the authorities if necessary, and prevent you from viewing any images hosted on Imgur.com. We mean it.`
+
+          }
         }
       })
     }
@@ -87,8 +92,10 @@
   <Modal onEscape={decline}>
     <Content>
       <h1 class="roboto text-2xl">Confirm File Upload</h1>
-      <p>Please accept the following terms:</p>
-      <p>{quote.terms}</p>
+      <p>Please accept the following <a href="https://imgur.com/tos">Imgur terms</a>:</p>
+      <p>{quote.termsLine1}</p>
+      <p>{quote.termsLine2}</p>
+      <p>{quote.termsLine3}</p>
       <div class="flex gap-2">
         <Anchor type="button" on:click={decline} {loading}>Decline</Anchor>
         <Anchor type="button-accent" on:click={accept} {loading}>Accept</Anchor>
